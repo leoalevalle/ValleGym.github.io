bd4d92bb8040ebe178b10c5e05ca20e2eb501fcb
leoalevalle2014 <leoalevalle2014@gmail.com>
2025-04-11 19:01:55 -0300

    correciones

--- js/style.js
+++ js/style.js
@@ -5,4 +5,31 @@ toggleBtn.addEventListener('click', () => {
     ? '☀️'
     : '🌙';
 });
+const counters = document.querySelectorAll('.counter');
 
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-target');
+      const current = +counter.innerText;
+      const increment = target / 100;
+
+      if (current < target) {
+        counter.innerText = Math.ceil(current + increment);
+        setTimeout(updateCount, 30);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    // Activar después de que aparezcan
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          updateCount();
+          observer.unobserve(entry.target);
+        }
+      });
+    }, { threshold: 0.5 });
+
+    observer.observe(counter);
+  });
